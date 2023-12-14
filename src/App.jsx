fb917b8bc8887c56669c49a9309c2574cab13f47
Zrazhevskii <vbn-134@yandex.ru>
2023-12-14 16:19:32 +0300

    добавление posts, menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+import { Menu } from './components/Menu'
+import { Posts } from './pages/Posts'
 import './App.css'
 
 function App() {
 
   return (
-    <>
+    <div>
+      <Routes>
+        <Route path='/' element={<Menu />}>
+            <Route index element={<Posts/>}/>
+        </Route>
+      </Routes>
       
-    </>
+    </div>
   )
 }
 
---
+++ src/components/Menu.jsx
@@ -0,0 +1,10 @@
+import { NavLink, Outlet } from 'react-router-dom'
+import React from 'react'
+
+export const Menu = () => {
+  return (
+    <div>
+        <NavLink to='/' />
+    </div>
+  )
+}
